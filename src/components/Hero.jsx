23bca7e6136d770efea6fa8508c2d096047cf495
surazgaundel <gaundelsuraz99@gmail.com>
2023-07-23 10:32:40 +0530

    Change the hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,20 +4,20 @@ import OnlinePayImage from '../assets/Online pay.svg'
 export default function Hero() {
   return (
     <main>
-        <div className="flex flex-col w-[90%] m-auto md:flex-row">
+        <div className="flex flex-col-reverse w-[90%] m-auto md:flex-row">
         <article className='hero-info md:w-[70%] md:m-auto mb-3'>
           <h1 className='text-[var(--yellow)] text-4xl mb-2'>
             Payments infrastructure <br />
             for the internet
           </h1>
-          <p className='text-xl mb-2'>
+          <p className='text-xl mb-2 leading-8'>
             Millions of companies of all sizes—from startups to Fortune 500s—use
             Ztripe&apos;s software and APIs to accept payments, send payouts, and
             manage their businesses online.
           </p>
           <button className='bg-[var(--blue)] px-2 py-1 rounded-md'>Start now</button>
         </article>
-        <article className=' w-[90%] m-auto md:w-1/2'>
+        <article className=' w-[80%] m-auto md:w-1/2'>
           <img src={OnlinePayImage} className='pay-img' alt='pay online' />
         </article>
         </div>
